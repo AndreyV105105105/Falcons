147307657019d575e2be6497f92b29a9c7f9ef06
volva <volvachandrey108@gmail.com>
2026-05-04 13:05:25 +0300

    add protect passwords

--- backend/transport/cloud_function.py
+++ backend/transport/cloud_function.py
@@ -1,6 +1,5 @@
 import json
 import os
-import base64
 from core.auth_service import register_user_logic, login_user_logic
 from core.generator_service import generate_logic
 
@@ -8,10 +7,18 @@ from core.presets_service import create_preset, get_presets, delete_preset
 from core.security import verify_token
 
 
+from core.passwords_service import (save_password_handler, get_passwords_handler, decrypt_password_handler,
+                                    delete_password_handler)
+
+from core.logger import logger
+
 def handler(event, context):
     """Единая точка входа для авторизации в Yandex Cloud"""
     try:
         # Ловим метод запроса (POST, GET, OPTIONS)
+        path = event.get('path', '')
+        http_method = event.get('httpMethod', '')
+        logger.info(f"Входящий запрос: {http_method} {path}")
         http_method = event.get('httpMethod', '')
 
         if http_method == 'OPTIONS':
@@ -29,8 +36,12 @@ def handler(event, context):
 
         # Расшифровка Base64
         raw_body = event.get('body', '{}')
-        if event.get('isBase64Encoded'):
-            raw_body = base64.b64decode(raw_body).decode('utf-8')
+        # Безопасный парсинг JSON (защита от пустых GET-запросов)
+        try:
+            # Если raw_body пустой, делаем его '{}', чтобы loads не упал
+            body = json.loads(raw_body) if raw_body else {}
+        except json.JSONDecodeError:
+            body = {}
 
         body = json.loads(raw_body)
         secret_key = os.getenv('JWT_SECRET', 'super-secret-key')
@@ -69,6 +80,22 @@ def handler(event, context):
                 return get_presets(user_id)
             elif '/presets/delete' in path:
                 return delete_preset(user_id, body)
+
+        elif '/passwords' in path:
+
+            if '/passwords/save' in path:
+                return save_password_handler(event)
+
+            elif '/passwords/get' in path:
+                return get_passwords_handler(event)
+
+            elif '/passwords/decrypt' in path:
+                return decrypt_password_handler(event)
+
+            elif '/passwords/delete' in path:
+                return delete_password_handler(event)
+
+
         else:
             return {
                 'statusCode': 404,
@@ -88,7 +115,7 @@ def handler(event, context):
             'body': json.dumps(result)
         }
     except Exception as e:
-        print(f"CRITICAL ERROR: {str(e)}")
+        logger.error(f"CRITICAL ERROR: {str(e)}", exc_info=True)
         return {
             'statusCode': 500,
             'headers': {'Access-Control-Allow-Origin': '*'},
